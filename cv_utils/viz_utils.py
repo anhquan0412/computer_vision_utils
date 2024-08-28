ad24821b5645a65cf569a85e41a769d1258d155f
Quan Tran <anhquan0412@gmail.com>
2024-08-29 01:35:15 +0800

    optional labels for viz_images

--- cv_utils/viz_utils.py
+++ cv_utils/viz_utils.py
@@ -2,12 +2,12 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import math
 
-def visualize_images(image_paths, labels, figsize=(10, 10), fontsize=8):
+def visualize_images(image_paths, labels=None, figsize=(10, 10), fontsize=8):
     # Determine grid size based on the number of images
     num_images = len(image_paths)
     grid_size = math.ceil(math.sqrt(num_images))
 
-    if not isinstance(labels,list):
+    if labels is not None and not isinstance(labels,list):
         labels = [labels for _ in range(num_images)]
     # Create the plot with specified figure size
     fig, axs = plt.subplots(grid_size, grid_size, figsize=figsize)
@@ -22,7 +22,8 @@ def visualize_images(image_paths, labels, figsize=(10, 10), fontsize=8):
     for i in range(num_images):
         img = mpimg.imread(image_paths[i])
         axs[i].imshow(img)
-        axs[i].set_title(f'{labels[i]}', fontsize=fontsize)
+        if labels is not None:
+            axs[i].set_title(f'{labels[i]}', fontsize=fontsize)
         axs[i].axis('off')
     
     # Hide any unused subplots if the grid is larger than the number of images
