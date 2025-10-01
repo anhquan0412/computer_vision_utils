b5aeded5e468206f642bd0625b2eed6370512a89
Quan Tran <anhquan0412@gmail.com>
2025-10-02 00:23:06 +0800

    change download_blob logic for download_img

--- cv_utils/img_utils.py
+++ cv_utils/img_utils.py
@@ -11,7 +11,7 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 from tqdm import tqdm 
 import logging
 import collections
-from datetime import timedelta, datetime, timezone
+from datetime import timedelta, datetime
 from pathlib import Path
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -49,9 +49,8 @@ def load_image_general(input_file: Union[str, BytesIO]) -> Image.Image:
 def download_img(img_file,input_container_client,ignore_exif_rotation=True,load_img=True):
     use_url = img_file.startswith(('http://', 'https://'))
     if not use_url and input_container_client is not None:
-        downloader = input_container_client.download_blob(img_file)
-        img_file = io.BytesIO()
-        blob_props = downloader.download_to_stream(img_file)
+        downloader = input_container_client.download_blob(img_file).readall()
+        img_file = io.BytesIO(downloader)
 
     img = md_viz.open_image(img_file,ignore_exif_rotation=ignore_exif_rotation)
     if load_img:
