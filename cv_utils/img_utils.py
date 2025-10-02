6fb4cd3bca2f1dd6474f11f4883cbd2044b22d59
Quan Tran <anhquan0412@gmail.com>
2025-10-02 09:26:54 +0800

    revert

--- cv_utils/img_utils.py
+++ cv_utils/img_utils.py
@@ -49,8 +49,9 @@ def load_image_general(input_file: Union[str, BytesIO]) -> Image.Image:
 def download_img(img_file,input_container_client,ignore_exif_rotation=True,load_img=True):
     use_url = img_file.startswith(('http://', 'https://'))
     if not use_url and input_container_client is not None:
-        downloader = input_container_client.download_blob(img_file).readall()
-        img_file = io.BytesIO(downloader)
+        downloader = input_container_client.download_blob(img_file)
+        img_file = io.BytesIO()
+        blob_props = downloader.download_to_stream(img_file)
 
     img = md_viz.open_image(img_file,ignore_exif_rotation=ignore_exif_rotation)
     if load_img:
